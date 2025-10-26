8c3a0817bc2976d940e6c33dc1d13d1b20128a13
saraelhark <41480355+saraelhark@users.noreply.github.com>
2025-10-26 20:38:16 +0100

    setup shadcn w nuxt

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <div class="bg-blue-500 text-red-500 p-6 mx-auto">Hello world</div>
+    <div class="container mx-auto p-8 space-y-4">
+        <h1 class="text-3xl font-bold mb-6">Circuit Scope</h1>
+
+        <!-- Default Button -->
+        <div class="space-x-2">
+            <Button>Default Button</Button>
+            <Button variant="secondary">Secondary</Button>
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from '~/components/ui/button'
+</script>
